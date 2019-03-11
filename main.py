d663094bf9cd80b1fe039bab29b9336d89ff3e91
hyunseok4384 <hyunseok4384@naver.com>
2019-03-11 21:15:20 +0900

    pyqt5 사용법

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from lib.YouViewerLayout import Ui_MainWindow
 from lib.AuthDialog import AuthDialog
 import datetime
 import re
+import pytube
 
 #form_class = uic.loadUiType(r"C:\section6\ui\you_viewer_v1.0.ui")[0]
 
@@ -26,6 +27,10 @@ class Main(QMainWindow, Ui_MainWindow):
 
         #재생 여부
         self.is_play = False
+
+        #Youtube관련 작업
+        self.youtb = None
+        self.youtb_fsize = 0
 
     #기본 UI 비활성화
     def initAuthLock(self):
@@ -51,10 +56,47 @@ class Main(QMainWindow, Ui_MainWindow):
     def showStatusMsg(self,msg):
         self.statusbar.showMessage(msg)
 
+    #시그널 초기화
     def initSignal(self):
         self.loginButton.clicked.connect(self.authCheck)
         self.previewButton.clicked.connect(self.load_url)
         self.exitButton.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        self.webEngineView.loadProgress.connect(self.showProgressBrowserLoading)
+        self.fileNavButton.clicked.connect(self.selectDownPath)
+        self.calendarWidget.clicked.connect(self.append_date)
+        self.startButton.clicked.connect(self.downloadYoutb)
+
+    @pyqtSlot()
+    def append_date(self):
+        cur_date = self.calendarWidget.selectedDate()
+        #print("clicl date", self.calendarWidget.selectedDate().toString())
+        print(str(cur_date.year())+"-"+str(cur_date.month())+"-"+str(cur_date.day()))
+        self.append_log_msg("Calender Click")
+
+    @pyqtSlot()
+    def downloadYoutb(self):
+        down_dir = self.pathTextEdit.text().strip()
+        if down_dir is None or down_dir == "" or not down_dir:
+            QMessageBox.about(self,"경로 선택","다운로드 받을 경로를 선택하세요")
+            #self.pathTextEdit.setFocus(True)
+            return None
+
+        self.youtb_fsize = self.youtb[self.streamCombobox.currentIndex()].filesize
+        print("fsize", self.youtb_fsize)
+        self.youtb_fsize = self.youtb[self.streamCombobox.currentIndex()].download(down_dir)
+        self.append_log_msg("Down Click")
+
+
+    @pyqtSlot()
+    def selectDownPath(self):
+        #파일선택
+        #fname = QFileDialog.getOpenFileName(self)
+        #self.pathTextEdit.setText(fname[0])
+
+        #경로선택
+        fpath = QFileDialog.getExistingDirectory(self,"Select Directory")
+        self.pathTextEdit.setText(fpath)
+
 
     def append_log_msg(self,act):
         now = datetime.datetime.now()
@@ -93,7 +135,16 @@ class Main(QMainWindow, Ui_MainWindow):
         url = self.urlTextEdit.text().strip()
         v = re.compile('^https://www.youtube.com/?')
         if self.is_play:
-            pass
+            self.append_log_msg("Stop Click")
+            self.webEngineView.load(QUrl("about:blank"))
+            self.previewButton.setText("재생")
+            self.is_play = False
+            self.urlTextEdit.clear()
+            self.urlTextEdit.setFocus(True)
+            self.startButton.setEnabled(False)
+            self.streamCombobox.clear()
+            self.progressBar_2.setValue(0)
+            self.showStatusMsg("인증 완료")
         else:
             if v.match(url) is not None:
                 self.append_log_msg("Play Click")
@@ -102,11 +153,37 @@ class Main(QMainWindow, Ui_MainWindow):
                 self.previewButton.setText("중지")
                 self.is_play = True
                 self.startButton.setEnabled(True)
+                self.initialYouWork(url)
+
             else:
                 QMessageBox.about(self,"URL 형식 오류","유튜브 주소형식이 아닙니다")
                 self.urlTextEdit.clear()
                 self.urlTextEdit.setFocus(True)
 
+    def initialYouWork(self, url):
+        video_list = pytube.YouTube(url)
+        #로딩바 계산
+
+        self.youtb = video_list.streams.all()
+        self.streamCombobox.clear()
+        for q in self.youtb:
+            #print("step1", q.itag, q.mime_type, q.abr)
+            tmp_list, str_list = [], []
+            tmp_list.append(str(q.mime_type or ""))
+            tmp_list.append(str(q.res or ""))
+            tmp_list.append(str(q.fps or ""))
+            tmp_list.append(str(q.abr or ""))
+
+            #print(tmp_list)
+            str_list = [x for x in tmp_list if x != ""]
+            #print("step3", str_list)
+
+            #print("join", ",".join(str_list))
+            self.streamCombobox.addItem(",".join(str_list))
+
+    @pyqtSlot(int)
+    def showProgressBrowserLoading(self, v):
+        self.progressBar.setValue(v)
 
 
 
